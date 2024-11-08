80f80bfbf1db67a4cce7455a83130d37f7de98f9
Ahmed Habib <ibfg00@gmail.com>
2024-11-08 11:14:22 +0200

    fix: dynamic work feature to static page

--- src/app/(root)/work/page.tsx
+++ src/app/(root)/work/page.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
-  return (
-    <div>
-      work page
-    </div>
-  )
-}
+  return <div>work page</div>;
+};
 
-export default page
+export default page;
